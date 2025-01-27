d8e3ea7ad9a4ca7b8463f1370bf21f09a35d934e
AJFintastic <akshar@fintasticdata.com>
2025-01-27 19:40:49 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,19 +1,11 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Sidebar from './components/Sidebar';
 import Dashboard from './pages/Dashboard';
 
 function App() {
   return (
-    <Router>
-      <div style={{ display: 'flex' }}>
-        <Sidebar />
-        <Routes>
-          {/* Define routes */}
-          <Route path="/dashboard" element={<Dashboard />} />
-        </Routes>
-      </div>
-    </Router>
+    <div>
+      <Dashboard />
+    </div>
   );
 }
 
